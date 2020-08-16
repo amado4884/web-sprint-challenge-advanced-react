08e26a1577e3208dcd8ff4c8f2e6ce0da8465b52
Amado Guerrero <amado4884@gmail.com>
2020-08-16 19:56:39 -0400

    finished mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { act } from "react-dom/test-utils";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+const setup = () => {
+  return render(<CheckoutForm />);
+};
 
-test("form shows success message on submit with form details", () => {});
+test("form header renders", () => {
+  const form = setup();
+  const header = form.queryByText(/Checkout Form/i);
+
+  expect(header.textContent).toBe("Checkout Form");
+});
+
+test("form shows success message on submit with form details", async () => {
+  const form = setup();
+  const button = form.getByText("Checkout");
+
+  await act(async () => {
+    await fireEvent.click(button);
+    const successMessage = form.getByTestId("successMessage");
+    expect(successMessage.textContent).toContain(
+      "You have ordered some plants! Woo-hoo! 🎉Your new green friends will be shipped to"
+    );
+  });
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,17 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from "react";
+
+const useForm = ({ firstName, lastName, address, city, state, zip }) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState({ firstName, lastName, address, city, state, zip });
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+  return [values, showSuccessMessage, setShowSuccessMessage, handleChanges, handleSubmit];
+};
+
+export default useForm;
